54dccdade1c5b13c455e200401d190561a726496
umair-jami <lalaumair92@gmail.com>
2024-11-10 20:45:50 +0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid bg-slate-600 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" text-center size-1/2 bg-gradient-to-tr to to-blue-500 from-red-400 m-auto">
 
         <h1 className="text-[80px] mt-96 text-white">Hello world!</h1>
-        <p className="text-red-400 mt-32">Hello world I am Studing at governer House Sindh Today we Practise first Step of Next.js</p>
+        <p className="text-white-400">Hello world I am Studing at governer House Sindh Today we Practise first Step of Next.js</p>
 
     </div>
   );
